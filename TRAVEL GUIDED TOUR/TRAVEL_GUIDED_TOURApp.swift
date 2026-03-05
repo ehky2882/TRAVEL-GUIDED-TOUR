22e00d4afc2a5ee05a6e6dc080e39e27e7c33007
kiubert2882 <edward.yung@gmail.com>
2026-03-04 21:49:48 -0500

    Initial Commit

---
+++ TRAVEL GUIDED TOUR/TRAVEL_GUIDED_TOURApp.swift
@@ -0,0 +1,17 @@
+//
+//  TRAVEL_GUIDED_TOURApp.swift
+//  TRAVEL GUIDED TOUR
+//
+//  Created by Edward Yung on 3/4/26.
+//
+
+import SwiftUI
+
+@main
+struct TRAVEL_GUIDED_TOURApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
